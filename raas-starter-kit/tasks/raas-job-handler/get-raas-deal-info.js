fbccc292a4808b4a019929925b2b91aa62bef31f
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-09-27 11:34:24 +0530

    fix: Changed RATEs in contract

--- raas-starter-kit/tasks/raas-job-handler/get-raas-deal-info.js
+++ raas-starter-kit/tasks/raas-job-handler/get-raas-deal-info.js
@@ -1,13 +1,11 @@
-task("deposit-raas-funds", "Whitelists a new worker to the deal client")
+task("get-raas-deal", "Fetches the RAAS deal data")
     .addParam("contract", "The address of the deal client solidity")
     .addParam("cidcontract", "The address of the deal client solidity")
     .addParam("cid", "The CID of the file for which deposit is initialised")
-    // .addParam("depositamount", "The address of the deal client solidity")
     .setAction(async (taskArgs) => {
         const cid = taskArgs.cid
         const contractAddr = taskArgs.contract
         const CIDcontractAddr = taskArgs.cidcontract
-        // const depositAmount = taskArgs.depositAmount
 
         //create a new wallet instance
         const wallet = new ethers.Wallet(network.config.accounts[0], ethers.provider)
@@ -25,11 +23,8 @@ task("deposit-raas-funds", "Whitelists a new worker to the deal client")
         //create a DealClient contract instance
         //this is what you will call to interact with the deployed contract
         const dealClient = await DealClient.attach(contractAddr)
-        // const amountInWei = ethers.utils.parseEther(`${depositAmount}`)
-        console.log("Depositing funds for the CID worth")
-        const transaction = await dealClient.depositFunds(ethers.utils.toUtf8Bytes(cid), {
-            value: ethers.utils.parseEther("2"),
-        })
-        await transaction.wait()
-        console.log("The Funds are now deposited in the contract")
+
+        const response = await dealClient.getRaasData(ethers.utils.toUtf8Bytes(cid))
+
+        console.log("Raas Deal info", response)
     })
