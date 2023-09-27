e2be4ebe75f0320189b003b874147ed197b963fc
Architsharma7 <architsharma7911@gmail.com>
2023-09-27 21:11:57 +0530

    feat: raas attacher added

--- frontend/pages/dashboard/[cid].js
+++ frontend/pages/dashboard/[cid].js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { getPODSIdetails } from "../../components/raasApiMethods";
+import {
+  getPODSIdetails,
+  registerRaasJob,
+} from "../../components/raasApiMethods";
 import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 import { parseEther, toBytes, stringToBytes, toHex } from "viem";
 import {
@@ -26,11 +29,20 @@ import {
   AccordionPanel,
   AccordionIcon,
 } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 
 const Cid = () => {
   const router = useRouter();
   const { cid } = router.query;
   const [podsiDealInfo, setPodsiDealInfo] = useState([]);
+  const [minerLocations, setMinerLocation] = useState(null);
+  const [raasjob, setRaasJob] = useState({
+    type: "",
+    copies: "",
+    enddate: "",
+    aggregator: "lighthouse",
+    epochs: "",
+  });
 
   const { address: account } = useAccount();
   const publicClient = usePublicClient();
@@ -51,36 +63,38 @@ const Cid = () => {
 
   const getpodsiData = async () => {
     const data = await getPODSIdetails(cid);
-    console.log(data)
+    console.log(data);
     setPodsiDealInfo(data);
     console.log(data);
   };
 
-//   const getMinerLocation = async () => {
-//     try {
-//       if (miner) {
-//         const doc = await DB.collection("ground_control_sp_location")
-//           .where("provider", "==", `${miner}`)
-//           .limit(1)
-//           .get();
-//         let finalData = [];
-//         await doc.data.forEach((e) => {
-//           finalData.push(e.data);
-//         });
-//         console.log(finalData);
-//         setMinerLocation(finalData);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+  const getMinerLocation = async () => {
+    try {
+      if (podsiDealInfo.dealInfo) {
+        const doc = await DB.collection("ground_control_sp_location")
+          .where("provider", "==", `${podsiDealInfo.dealInfo.storageProvider}`)
+          .limit(1)
+          .get();
+        let finalData = [];
+        await doc.data.forEach((e) => {
+          finalData.push(e.data);
+        });
+        console.log(finalData);
+        setMinerLocation(finalData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  
   return (
     <div>
       <div className="w-screen">
-        <div className="w-11/12 flex justify-center mx-auto mt-20">
-          <div className="w-1/2 mx-2">
+        <div className="flex justify-center mx-auto mt-10">
+          <p className="text-3xl text-blue-500">File Details and RAAS</p>
+        </div>
+        <div className="w-11/12 flex justify-center mx-auto mt-6">
+          <div className="w-1/2 mt-10 mx-5">
             <div className="flex flex-col">
               <TableContainer className="mb-4 justify-center mx-auto">
                 <Table variant="simple">
@@ -107,7 +121,7 @@ const Cid = () => {
                     </Tr>
                     <Tr>
                       <Td>Geo Location of Miner</Td>
-                      <Td>metres (m)</Td>
+                      <Td>{minerLocations ? minerLocations : `null`}</Td>
                     </Tr>
                   </Tbody>
                 </Table>
@@ -126,7 +140,7 @@ const Cid = () => {
                     <TableContainer className="">
                       <Table variant="simple">
                         <Tbody>
-                          <Tr>
+                          {/* <Tr>
                             <Td>Car File Size</Td>
                            <Td>{podsiDealInfo && podsiDealInfo.carFileSize}</Td>
                           </Tr>
@@ -153,7 +167,7 @@ const Cid = () => {
                           <Tr>
                             <Td>Verifier ID</Td>
                             <Td>{podsiDealInfo && podsiDealInfo.proof.fileProof.verifierData.commPc}</Td>
-                          </Tr>
+                          </Tr> */}
                         </Tbody>
                       </Table>
                     </TableContainer>
@@ -162,7 +176,113 @@ const Cid = () => {
               </Accordion>
             </div>
           </div>
-          <div className="w-1/2 mx-5">{}</div>
+          <Divider orientation="vertical" />
+          <div className="w-1/2 mt-10 mx-14">
+            <div className="flex flex-col">
+              <Card>
+                <CardBody>
+                  <div className="flex flex-col">
+                    <p className="text-blue-500 text-2xl text-center">
+                      Attach RAAS
+                    </p>
+                    <div className="mt-5">
+                      <p className="text-xl">Job Type</p>
+                      <select
+                        value={raasjob.type}
+                        onChange={(e) => {
+                          setRaasJob({
+                            ...raasjob,
+                            type: e.target.value,
+                          });
+                        }}
+                        className="mt-4 text-xl w-full bg-gray-100 px-3 py-2 rounded-xl cursor-pointer"
+                      >
+                        <option value={"Replication"}>Replication</option>
+                        <option value={"Renew"}>Renew</option>
+                        <option value={"Repair"}>Repair</option>
+                      </select>
+                      <div className="mt-5">
+                        <p className="text-xl">Number of Copies</p>
+                        <input
+                          onChange={(e) => {
+                            setRaasJob({
+                              ...raasjob,
+                              copies: e.target.value,
+                            });
+                          }}
+                          className="border border-black mt-4 px-3 w-full py-1.5 rounded-xl"
+                          type="number"
+                          placeholder=""
+                          value={raasjob.copies}
+                        ></input>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-xl">End Date</p>
+                        <input
+                          onChange={(e) => {
+                            setRaasJob({
+                              ...raasjob,
+                              enddate: e.target.value,
+                            });
+                          }}
+                          className="border border-black mt-4 px-3 w-full py-1.5 rounded-xl"
+                          type="date"
+                          placeholder=""
+                          value={raasjob.enddate}
+                        ></input>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-xl">Aggregator</p>
+                        <input
+                          onChange={(e) => {
+                            setRaasJob({
+                              ...raasjob,
+                              aggregator: e.target.value,
+                            });
+                          }}
+                          className="border border-black mt-4 px-3 w-full py-1.5 rounded-xl"
+                          type="text"
+                          placeholder=""
+                          value={raasjob.aggregator}
+                        ></input>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-xl">Epochs</p>
+                        <input
+                          onChange={(e) => {
+                            setRaasJob({
+                              ...raasjob,
+                              epochs: e.target.value,
+                            });
+                          }}
+                          className="border border-black mt-4 px-3 w-full py-1.5 rounded-xl"
+                          type="number"
+                          placeholder=""
+                          value={raasjob.epochs}
+                        ></input>
+                      </div>
+                      <div className="mt-5 w-full flex">
+                        <button
+                          onClick={() =>
+                            registerRaasJob(
+                              cid,
+                              raasjob.enddate,
+                              raasjob.copies,
+                              raasjob.aggregator,
+                              raasjob.epochs
+                            )
+                          }
+                          className="text-blue-500 border border-blue-500 px-10 py-2 rounded-xl mx-auto text-center items-center"
+                        >
+                          Attach
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
         </div>
       </div>
     </div>
