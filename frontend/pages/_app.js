94047ea5fc86d18f02a4a8990ac2be5388667d96
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-09-27 14:16:12 +0530

    feat: raasAPI Handler functions added with viem and contracts interactions

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -10,9 +10,31 @@ import Navbar from "../components/navbar";
 import localFont from "@next/font/local";
 
 const myFont = localFont({ src: "./CalSans-SemiBold.woff2" });
+const CalibrationChain = {
+  id: 314159,
+  name: "Filecoin - Calibration testnet",
+  network: "Filecoin - Calibration testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Test Filecoin",
+    symbol: "tFIL",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://calibration.filfox.info/rpc/v1"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Filfox Explorer",
+      url: "https://calibration.filfox.info",
+    },
+  },
+  testnet: true,
+};
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, polygonMumbai],
+  [CalibrationChain],
   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
 );
 
